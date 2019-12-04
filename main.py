68e2c5203d75fe2c1c26327373db825703b4973a
LachlanWalls <lachlan.walls1@gmail.com>
2019-12-04 17:39:09 +1100

    Added basic file uploading

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, url_for, redirect
 from pathlib import Path
 from werkzeug.utils import secure_filename
 import os
 
 # basic Flask setup
-app = Flask(__name__)
+app = Flask(__name__, template_folder="public")
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024 # one gig max file size
 
 
 # functions
 @app.route('/', methods=["GET"])
 def root():
-    return '<h3 style="text-align: center">lachlantransfer is coming soon</h3>'
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['uploads'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+
+    return render_template('index.html')
 
 
 @app.route('/public/<path:filename>')
