01c3c15417a0a22af9b91b4d7ba02e0fe2d791a5
LachlanWalls <lachlan.walls1@gmail.com>
2019-12-05 12:38:55 +1100

    Wrap in try/except

--- main.py
+++ main.py
@@ -13,18 +13,19 @@ app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024 # one gig max file size
 @app.route('/', methods=["GET", "POST"])
 def root():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            print(request)
-            return '{"error": "no file in request.files"}'
+        try:
+            if 'file' not in request.files:
+                return '{"error": "no file in request.files"}'
 
-        file = request.files['file']
-        if file.filename == '':
-            print(request)
-            return '{"error": "filename is empty"}'
+            file = request.files['file']
+            if file.filename == '':
+                return '{"error": "filename is empty"}'
 
-        filename = int(round(time.time() * 1000)) + "__" + secure_filename(file.filename)
-        file.save('/home/dynodelc/transfer.dynodel.com/uploads/' + filename)
-        return '{"filename": "' + filename + '"}'
+            filename = int(round(time.time() * 1000)) + "__" + secure_filename(file.filename)
+            file.save('/home/dynodelc/transfer.dynodel.com/uploads/' + filename)
+            return '{"filename": "' + filename + '"}'
+        except Exception as e:
+            return str(e)
 
     return render_template('index.html')
 
