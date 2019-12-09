fa359143efd36d0f25b0e072a6b628b1ef1024b7
LachlanWalls <lachlan.walls1@gmail.com>
2019-12-09 17:20:19 +1100

    Added language updating

---
+++ public/langs.js
@@ -0,0 +1,20 @@
+const LANGS = {
+    "en": {
+        "title": "lachlantransfer",
+        "send": "SEND",
+        "receive": "RECEIVE",
+        "choose": "Choose a file",
+        "drop": " or drop files here.",
+        "upload": "upload",
+        "UPLOAD": "UPLOAD",
+        "uploadsucc": "UPLOADED SUCCESSFULLY",
+        "uploadfail": "FAILED. TRY AGAIN?",
+        "download": "download",
+        "DOWNLOAD": "DOWNLOAD",
+        "type": "type",
+        "bytes": "bytes",
+        "kilobytes": "kilobytes",
+        "megabytes": "megabytes",
+        "nofiles": "No files to receive."
+    }
+}
--- public/script.js
+++ public/script.js
@@ -1,6 +1,64 @@
+function getCookie(cname) {
+    var ca = decodeURIComponent(document.cookie).split(';')
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i]
+        while (c.charAt(0) == ' ') c = c.substring(1)
+        if (c.indexOf(cname + "=") == 0) return c.substring((cname + "=").length, c.length)
+    }
+    return ""
+}
+
+function setCookie(cname, cvalue, exdays = 100000) {
+    var d = new Date()
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+    document.cookie = cname + "=" + cvalue + ";expires=" + d.toUTCString() + ";path=/";
+}
+
+let txt = LANGS["en"]
+
+function setLanguage(code) {
+    let lang = code || "en"
+    document.querySelector("html").setAttribute("lang", lang)
+    document.querySelector("#nav>select>option[value='" + lang + "']").setAttribute("selected", "")
+    txt = LANGS[lang]
+
+    setCookie("lang", code)
+
+    document.title = txt.title
+    document.querySelector("#nav>h4").innerHTML = txt.title
+    document.querySelector("#nav>.pages>.upload").innerHTML = txt.UPLOAD
+    document.querySelector("#nav>.pages>.download").innerHTML = txt.DOWNLOAD
+    document.querySelector("#quickstart>.upload").innerHTML = txt.UPLOAD
+    document.querySelector("#quickstart>.download").innerHTML = txt.DOWNLOAD
+    document.querySelector("#upload>.container>.addfile>.filedrag").innerHTML = "<strong>" + txt.choose + "</strong>" + txt.drop
+    document.querySelector("#download>.container>p").innerHTML = txt.nofiles
+
+    document.querySelector("#nav").setAttribute("show", "")
+    document.querySelector("#quickstart").setAttribute("show", "")
+    document.querySelector("#maincontainer").setAttribute("show", "")
+}
+
 (function() {
 
     let pagestart = Date.now()
+
+    // multilingual support
+    // the highest priority goes to parameters in the url
+    const urlParams = new URLSearchParams(window.location.search)
+    const langParam = urlParams.get('lang')
+    if (langParam && LANGS[langParam]) setLanguage(langParam)
+    else {
+        // second priority goes to COOKIES!!! i like cookie
+        let langcookie = getCookie("lang")
+        if (langcookie && LANGS[langcookie]) setLanguage(langcookie)
+        else {
+            // otherwise default to english
+            setLanguage("en")
+        }
+    }
+
+    // change language if the dropdown is changed, this will also update the cookie for future visits
+    document.querySelector("select").addEventListener("change", () => setLanguage(document.querySelector("select").value))
 
     function setPage(p, qs = false) {
         document.querySelector("#nav>.pages").setAttribute("page", p)
@@ -10,7 +68,7 @@
             document.querySelector("#upload").removeAttribute("hidden")
             document.querySelector("#download").removeAttribute("hidden")
             document.querySelector("#nav>.pages").removeAttribute("hidden")
-            document.querySelector("#quickstart").setAttribute("hidden", "")
+            document.querySelector("#quickstart").removeAttribute("show")
         }
     }
 
@@ -34,16 +92,13 @@
     }
 
     function addFile(file, files) {
-        // file.name file.type file.size (bytes)
-        // add the file to the thingo
-
         let div = document.createElement("div")
         div.className = "file"
         let id = Math.round(Math.random() * 10000)
         div.id = "upload_" + id
 
         let size = file.size
-        let exts = ["bytes", "kilobytes", "megabytes"]
+        let exts = [txt.bytes, txt.kilobytes, txt.megabytes]
         let ei = 0
         while (size > 1000) {
             if (ei < 2) {
@@ -52,7 +107,7 @@
             } else break
         }
 
-        div.innerHTML = "<h3>" + file.name + "</h3><p>type: " + file.type + "</p><p>" + size + " " + exts[ei] + "</p><input type='file' style='display: none'><button>UPLOAD</button><div class='loader' style='display: none;'><div class='progress'></div></div>"
+        div.innerHTML = "<h3>" + file.name + "</h3><p>" + txt.type + ": " + file.type + "</p><p>" + size + " " + exts[ei] + "</p><input type='file' style='display: none'><button>" + txt.UPLOAD + "</button><div class='loader' style='display: none;'><div class='progress'></div></div>"
         document.querySelector("#upload>.container").insertBefore(div, document.querySelector("#upload>.container>.addfile"))
         document.querySelector("#upload_" + id + ">input").files = files
 
@@ -73,20 +128,20 @@
             }).then(data => {
                 e.target.style.display = "block"
                 e.target.parentElement.querySelector(".loader").style.display = "none"
-                e.target.innerHTML = "UPLOADED SUCCESSFULLY"
+                e.target.innerHTML = txt.UPLOADsucc
                 e.target.style.pointerEvents = "none"
 
                 let download = document.createElement("a")
                 download.className = "download"
                 download.href = "/uploads/" + data.data.filename
                 download.setAttribute("download", data.data.filename.split("/").pop())
-                download.innerHTML = "download"
+                download.innerHTML = txt.download
                 e.target.parentElement.appendChild(download)
 
             }).catch(error => {
                 e.target.style.display = "block"
                 e.target.parentElement.querySelector(".loader").style.display = "none"
-                e.target.innerHTML = "FAILED. TRY AGAIN?"
+                e.target.innerHTML = txt.UPLOADfail
             })
         })
 
@@ -106,11 +161,11 @@
     }
 
     function displayReceivingFiles(files) {
-        document.querySelector("#download>.container").innerHTML = (files.length == 0) ? "<p>No files to receive.</p>" : ""
+        document.querySelector("#download>.container").innerHTML = (files.length == 0) ? "<p>" + txt.nofiles + "</p>" : ""
         files.forEach(file => {
             let div = document.createElement("div")
             div.className = "file"
-            div.innerHTML = "<h3>" + file.split("__")[1] + "</h3><a href='/uploads/" + file + "' download='" + file + "'>download</a>"
+            div.innerHTML = "<h3>" + file.split("__")[1] + "</h3><a href='/uploads/" + file + "' download='" + file + "'>" + txt.download + "</a>"
             document.querySelector("#download>.container").appendChild(div)
         })
     }
